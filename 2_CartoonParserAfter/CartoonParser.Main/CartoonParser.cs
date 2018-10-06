10e8f601d39c1cfbc8040303b573b196ce8f6fc8
Kevin Garner <kevinleegarner@gmail.com>
2018-10-06 09:04:48 -0500

    Renamed "data" to "text" to make a meaningful distinction in our code. It's slight, but data is so generic that it could be anything, and we're specifically asking for text to parse into cartoon objects.

--- 2_CartoonParserAfter/CartoonParser.Main/CartoonParser.cs
+++ 2_CartoonParserAfter/CartoonParser.Main/CartoonParser.cs
@@ -30,15 +30,15 @@ namespace CartoonParser.Main
         private const char GenreDelimiter = ',';
 
         /// <summary>
-        /// Parses cartoon data from a single format. All fields are required.
+        /// Parses cartoon text from a single format. All fields are required.
         /// </summary>
-        /// <param name="data">The data</param>
+        /// <param name="text">The text</param>
         /// <returns>A list of cartoons</returns>
-        public IList<Cartoon> Parse(string data)
+        public IList<Cartoon> Parse(string text)
         {
             /* Beginning */
             var cartoons = new List<Cartoon>();
-            using (var stringReader = new StringReader(data))
+            using (var stringReader = new StringReader(text))
             {
                 /* Line Parsing */
                 string line;
@@ -118,71 +118,71 @@ namespace CartoonParser.Main
                                 break;
                             // Handle version 2 file lines
                             //case "2.0":
-                                //// Validate the data
-                                //if (row.Split('|').Length == 5)
-                                //{
-                                //    // File must have valid name
-                                //    if (!string.IsNullOrWhiteSpace(row.Split('|')[0]))
-                                //    {
-                                //        // File must have valid release date
-                                //        if (DateTime.TryParse(row.Split('|')[1], out var _))
-                                //        {
-                                //            // File must have valid studio
-                                //            if (!string.IsNullOrWhiteSpace(row.Split('|')[2]))
-                                //            {
-                                //                // File must have non-empty genre list with non-empty entries
-                                //                if (!string.IsNullOrWhiteSpace(row.Split('|')[3]) && row.Split('|')[3].Split(',').All(x => !string.IsNullOrEmpty(x)) && row.Split('|')[3].Split(',').GroupBy(x => x).Any(x => x.Any()))
-                                //                {
-                                //                    // File must have valid IMDB score
-                                //                    if (decimal.TryParse(row.Split('|')[4], out var _))
-                                //                    {
-                                //                        // add the cartoon
-                                //                        cartoons.Add(new Cartoon
-                                //                        {
-                                //                            //Map the values
-                                //                            Name = row.Split('|')[0],
-                                //                            ReleaseDate = DateTime.Parse(row.Split('|')[1]),
-                                //                            Studio = row.Split('|')[2],
-                                //                            Genres = row.Split('|')[3].Split(','),
-                                //                            ImdbScore = Convert.ToDecimal(row.Split('|')[4])
-                                //                        });
-                                //                    }
-                                //                    else
-                                //                    {
-                                //                        // Throw validation exception on genre
-                                //                        throw new CartoonParserValidationException("Invalid Cartoon IMDB Score detected. Unable to parse file.");
-                                //                    } // End else for genre validation
-                                //                }
-                                //                else
-                                //                {
-                                //                    // Throw validation exception on genre
-                                //                    throw new CartoonParserValidationException("Invalid Cartoon Genre List detected. Unable to parse file.");
-                                //                } // End else for genre validation
-                                //            }
-                                //            else
-                                //            {
-                                //                // Throw validation exception on studio
-                                //                throw new CartoonParserValidationException("Invalid Cartoon Studio detected. Unable to parse file.");
-                                //            } // End else for studio validation
-                                //        }
-                                //        else
-                                //        {
-                                //            // Throw validation exception on release date
-                                //            throw new CartoonParserValidationException("Invalid Cartoon Release Date detected. Unable to parse file.");
-                                //        } // End else for release date validation
-                                //    }
-                                //    else
-                                //    {
-                                //        // Throw validation exception on name
-                                //        throw new CartoonParserValidationException("Invalid Cartoon Name detected. Unable to parse file.");
-                                //    } // End else for name validation
-                                //}
-                                //else
-                                //{
-                                //    // Throw validation exception on data length
-                                //    throw new CartoonParserValidationException("Incorrect number of segments in row data. Unable to parse file.");
-                                //} // End else for data length
-                                //break;
+                            //// Validate the data
+                            //if (row.Split('|').Length == 5)
+                            //{
+                            //    // File must have valid name
+                            //    if (!string.IsNullOrWhiteSpace(row.Split('|')[0]))
+                            //    {
+                            //        // File must have valid release date
+                            //        if (DateTime.TryParse(row.Split('|')[1], out var _))
+                            //        {
+                            //            // File must have valid studio
+                            //            if (!string.IsNullOrWhiteSpace(row.Split('|')[2]))
+                            //            {
+                            //                // File must have non-empty genre list with non-empty entries
+                            //                if (!string.IsNullOrWhiteSpace(row.Split('|')[3]) && row.Split('|')[3].Split(',').All(x => !string.IsNullOrEmpty(x)) && row.Split('|')[3].Split(',').GroupBy(x => x).Any(x => x.Any()))
+                            //                {
+                            //                    // File must have valid IMDB score
+                            //                    if (decimal.TryParse(row.Split('|')[4], out var _))
+                            //                    {
+                            //                        // add the cartoon
+                            //                        cartoons.Add(new Cartoon
+                            //                        {
+                            //                            //Map the values
+                            //                            Name = row.Split('|')[0],
+                            //                            ReleaseDate = DateTime.Parse(row.Split('|')[1]),
+                            //                            Studio = row.Split('|')[2],
+                            //                            Genres = row.Split('|')[3].Split(','),
+                            //                            ImdbScore = Convert.ToDecimal(row.Split('|')[4])
+                            //                        });
+                            //                    }
+                            //                    else
+                            //                    {
+                            //                        // Throw validation exception on genre
+                            //                        throw new CartoonParserValidationException("Invalid Cartoon IMDB Score detected. Unable to parse file.");
+                            //                    } // End else for genre validation
+                            //                }
+                            //                else
+                            //                {
+                            //                    // Throw validation exception on genre
+                            //                    throw new CartoonParserValidationException("Invalid Cartoon Genre List detected. Unable to parse file.");
+                            //                } // End else for genre validation
+                            //            }
+                            //            else
+                            //            {
+                            //                // Throw validation exception on studio
+                            //                throw new CartoonParserValidationException("Invalid Cartoon Studio detected. Unable to parse file.");
+                            //            } // End else for studio validation
+                            //        }
+                            //        else
+                            //        {
+                            //            // Throw validation exception on release date
+                            //            throw new CartoonParserValidationException("Invalid Cartoon Release Date detected. Unable to parse file.");
+                            //        } // End else for release date validation
+                            //    }
+                            //    else
+                            //    {
+                            //        // Throw validation exception on name
+                            //        throw new CartoonParserValidationException("Invalid Cartoon Name detected. Unable to parse file.");
+                            //    } // End else for name validation
+                            //}
+                            //else
+                            //{
+                            //    // Throw validation exception on data length
+                            //    throw new CartoonParserValidationException("Incorrect number of segments in row data. Unable to parse file.");
+                            //} // End else for data length
+                            //break;
                             default:
                                 throw new CartoonParserValidationException("No version detected. Unable to parse.");
                         }
