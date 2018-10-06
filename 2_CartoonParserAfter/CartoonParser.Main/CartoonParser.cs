6a445fef3f0ec2edadebdd5f6dcaaa4e30adba99
Kevin Garner <kevinleegarner@gmail.com>
2018-10-06 09:52:31 -0500

    Extracted the mapper to its own class. Extracted version 1 file specification fields to their own class.
    Added a constructor to the cartoon parser and required the mapper in the constructor.

--- 2_CartoonParserAfter/CartoonParser.Main/CartoonParser.cs
+++ 2_CartoonParserAfter/CartoonParser.Main/CartoonParser.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CartoonParser.Main.Exceptions;
+using static CartoonParser.Main.CartoonVersion1Specification;
 
 // TODO: Clean this code!
 namespace CartoonParser.Main
 {
+    public class CartoonMapper
+    {
+        public Cartoon Map(string line)
+        {
+            return new Cartoon
+            {
+                Name = line.Split(SegmentDelimiter)[SegmentIndexName],
+                ReleaseDate = DateTime.Parse(line.Split(SegmentDelimiter)[SegmentIndexReleaseDate]),
+                Studio = line.Split(SegmentDelimiter)[SegmentIndexStudio],
+                Genres = line.Split(SegmentDelimiter)[SegmentIndexGenres].Split(GenreDelimiter)
+            };
+        }
+    }
+
+    public static class CartoonVersion1Specification
+    {
+        public const int ValidSegmentLength = 4;
+        public const int SegmentIndexName = 0;
+        public const int SegmentIndexReleaseDate = 1;
+        public const int SegmentIndexStudio = 2;
+        public const int SegmentIndexGenres = 3;
+        public const string CartoonVersion1 = "1.0";
+        public const char SegmentDelimiter = '|';
+        public const char GenreDelimiter = ',';
+    }
+
     public class CartoonParser
     {
+        private readonly CartoonMapper _mapper;
         private List<Cartoon> _cartoons;
         private string _version;
-        private const int ValidSegmentLength = 4;
-        private const int SegmentIndexName = 0;
-        private const int SegmentIndexReleaseDate = 1;
-        private const int SegmentIndexStudio = 2;
-        private const int SegmentIndexGenres = 3;
-        private const string CartoonVersion1 = "1.0";
-        private const char SegmentDelimiter = '|';
-        private const char GenreDelimiter = ',';
+
+        public CartoonParser(CartoonMapper mapper)
+        {
+            _mapper = mapper;
+        }
 
         public IList<Cartoon> Parse(string text)
         {
@@ -47,13 +72,13 @@ namespace CartoonParser.Main
             return allLines
                 .Take(1)
                 .FirstOrDefault()
-                ?.Split(SegmentDelimiter)
+                ?.Split(CartoonVersion1Specification.SegmentDelimiter)
                 .LastOrDefault();
         }
 
         private static void ValidateVersion(string version)
         {
-            var versionIsValid = version == CartoonVersion1;
+            var versionIsValid = version == CartoonVersion1Specification.CartoonVersion1;
             if (!versionIsValid)
             {
                 throw new CartoonParserValidationException("Unable to read file. Unrecognized format.");
@@ -77,7 +102,7 @@ namespace CartoonParser.Main
         {
             switch (_version)
             {
-                case CartoonVersion1:
+                case CartoonVersion1Specification.CartoonVersion1:
                     ValidateAndMapAndAdd(line);
                     break;
                 default:
@@ -88,33 +113,33 @@ namespace CartoonParser.Main
         private void ValidateAndMapAndAdd(string line)
         {
             Validate(line);
-            var cartoon = Map(line);
+            var cartoon = _mapper.Map(line);
             _cartoons.Add(cartoon);
         }
 
         private static void Validate(string line)
         {
-            var segmentLengthIsValid = line.Split(SegmentDelimiter).Length == ValidSegmentLength;
+            var segmentLengthIsValid = line.Split(CartoonVersion1Specification.SegmentDelimiter).Length == CartoonVersion1Specification.ValidSegmentLength;
             if (!segmentLengthIsValid)
             {
                 throw new CartoonParserValidationException(
                     "Incorrect number of segments in row data. Unable to parse file.");
             }
 
-            var nameIsValid = !string.IsNullOrWhiteSpace(line.Split(SegmentDelimiter)[0]);
+            var nameIsValid = !string.IsNullOrWhiteSpace(line.Split(CartoonVersion1Specification.SegmentDelimiter)[0]);
             if (!nameIsValid)
             {
                 throw new CartoonParserValidationException("Invalid Cartoon Name detected. Unable to parse file.");
             }
 
-            var releaseDateIsValid = DateTime.TryParse(line.Split(SegmentDelimiter)[SegmentIndexReleaseDate], out _);
+            var releaseDateIsValid = DateTime.TryParse(line.Split(CartoonVersion1Specification.SegmentDelimiter)[CartoonVersion1Specification.SegmentIndexReleaseDate], out _);
             if (!releaseDateIsValid)
             {
                 throw new CartoonParserValidationException(
                     "Invalid Cartoon Release Date detected. Unable to parse file.");
             }
 
-            var studioIsValid = !string.IsNullOrWhiteSpace(line.Split(SegmentDelimiter)[SegmentIndexStudio]);
+            var studioIsValid = !string.IsNullOrWhiteSpace(line.Split(CartoonVersion1Specification.SegmentDelimiter)[CartoonVersion1Specification.SegmentIndexStudio]);
             if (!studioIsValid)
             {
                 throw new CartoonParserValidationException(
@@ -130,27 +155,16 @@ namespace CartoonParser.Main
 
         private static bool GenreIsValid(string line)
         {
-            var genresSegmentIsEmpty = string.IsNullOrWhiteSpace(line.Split(SegmentDelimiter)[SegmentIndexGenres]);
-            var oneGenreIsEmpty = !line.Split(SegmentDelimiter)[SegmentIndexGenres].Split(GenreDelimiter)
+            var genresSegmentIsEmpty = string.IsNullOrWhiteSpace(line.Split(CartoonVersion1Specification.SegmentDelimiter)[CartoonVersion1Specification.SegmentIndexGenres]);
+            var oneGenreIsEmpty = !line.Split(CartoonVersion1Specification.SegmentDelimiter)[CartoonVersion1Specification.SegmentIndexGenres].Split(CartoonVersion1Specification.GenreDelimiter)
                 .All(x => !string.IsNullOrEmpty(x));
-            var hasDuplicateGenre = !line.Split(SegmentDelimiter)[SegmentIndexGenres]
-                .Split(GenreDelimiter).GroupBy(x => x)
-                .All(x => x.Count() == SegmentIndexReleaseDate);
+            var hasDuplicateGenre = !line.Split(CartoonVersion1Specification.SegmentDelimiter)[CartoonVersion1Specification.SegmentIndexGenres]
+                .Split(CartoonVersion1Specification.GenreDelimiter).GroupBy(x => x)
+                .All(x => x.Count() == CartoonVersion1Specification.SegmentIndexReleaseDate);
 
             return !genresSegmentIsEmpty &&
                    !oneGenreIsEmpty &&
                    !hasDuplicateGenre;
-        }
-
-        private Cartoon Map(string line)
-        {
-            return new Cartoon
-            {
-                Name = line.Split(SegmentDelimiter)[SegmentIndexName],
-                ReleaseDate = DateTime.Parse(line.Split(SegmentDelimiter)[SegmentIndexReleaseDate]),
-                Studio = line.Split(SegmentDelimiter)[SegmentIndexStudio],
-                Genres = line.Split(SegmentDelimiter)[SegmentIndexGenres].Split(GenreDelimiter)
-            };
         }
     }
 
